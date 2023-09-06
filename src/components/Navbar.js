86b62d522f0e9d2ed972c94da2ab064437fec702
Mykel-Eze <michaeleze1997@gmail.com>
2023-09-06 07:38:12 +0100

    Mobile menu fixed

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,15 @@
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 import "../assets/css/navbar.css"
 
 const Navbar = () => {
+    const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+
+    const toggleMobileMenu = () => {
+        setIsMobileMenuOpen(!isMobileMenuOpen);
+    };
+
     return (
         <nav>
             <div className="container flex-div justify-content-btw">
@@ -31,9 +38,32 @@ const Navbar = () => {
                     </ul>
 
                     <div className="visible-xs">
-                        <img src={require("../assets/images/menu.svg").default} alt="menu" className="menu-trigger" />
+                        <img src={require("../assets/images/menu.svg").default} alt="menu" className={`menu-trigger open-menu ${isMobileMenuOpen ? 'open' : 'close'}`} onClick={toggleMobileMenu} />
+                        <img src={require("../assets/images/close.svg").default} alt="close" className={`menu-trigger close-menu ${isMobileMenuOpen ? 'open' : 'close'}`} onClick={toggleMobileMenu} />
                     </div>
                 </div>
+            </div>
+
+            <div className={`menu ${isMobileMenuOpen ? 'open' : ''}`}>
+                <ul className="mobile-menu-dropdown">
+                    <li onClick={toggleMobileMenu}>
+                        {/* <a href="/#how-it-works"></a> */}
+                        <Link to={"/#how-it-works"}>
+                            How it works
+                        </Link>
+                    </li>
+                    <li onClick={toggleMobileMenu}>
+                        <Link to={"/contact-us"}>
+                            Contact Us
+                        </Link>
+                    </li>
+                    <li>
+                        <div className="flex-div nav-country">
+                            <img src={require("../assets/images/Nigeria.svg").default} alt="Nigeria" />
+                            <span>NIGERIA</span>
+                        </div>
+                    </li>
+                </ul>
             </div>
         </nav>
     )
